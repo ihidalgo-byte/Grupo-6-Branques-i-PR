39dbe70c926146ede9df8f2ae0aa277bb366a4a7
Hugo <hcorreoso@nigulcide.es>
2026-02-13 16:54:58 +0100

    Media acabada

--- CalculadorNotes/src/Main.java
+++ CalculadorNotes/src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
+    public static double media (int sumaNotas,int numeroNotas){
+        double media = sumaNotas / numeroNotas;
+        return media;
+    }
     public static void main(String[] args) {
         System.out.println("Benvinguts al Calculador de Notes!");
     }
