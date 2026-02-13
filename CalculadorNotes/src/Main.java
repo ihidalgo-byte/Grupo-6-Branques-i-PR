219e91f6fab63b90a5b75a1ed5f0294bd430795b
ivan <ihidalgo@nigul.cide.es>
2026-02-13 15:44:55 +0100

    Notas Estudiantes

--- CalculadorNotes/src/Main.java
+++ CalculadorNotes/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Benvinguts al Calculador de Notes!");
+
+        System.out.print("Quants estudiants hi ha a la classe? ");
+        String[] assignaturas = {"Digitalització","Computer Systems","Llenguatges de Marques","Programació","Bases de Dades","Entorns de Desenvolupament"};
+        int numeroEstudiantes = sc.nextInt();
+        int numeroNotas = 6;
+        double sumaNotas = 0;
+
+        for (int i = 0; i < numeroEstudiantes; i++) {
+            int numeroEstudiante = i+1;
+            System.out.println("Estudiant numero " + numeroEstudiante + ":");
+
+            for (int i2 = 0; i2 < numeroNotas; i2++) {
+                System.out.print("Nota de " +assignaturas[i2]+ ":");
+                double nota = sc.nextDouble();
+                sumaNotas = nota + sumaNotas;
+            }
+        }
+        sc.close();
     }
 }
